adb9c177f8df1823e6a67554006178b1f7c7bdc3
Nikhilgujela <139757274+Nikhilgujela@users.noreply.github.com>
2025-07-03 18:18:28 +0530

    Update install.sh

--- deploy_scripts/install.sh
+++ deploy_scripts/install.sh
@@ -3,7 +3,7 @@ curl -sL https://rpm.nodesource.com/setup_lts.x | bash -
 yum install nodejs -y #default-jre ImageMagick
 
 # install pm2 module globaly
-npm install -g pm2
+sudo npm install -g pm2
 pm2 update
 
 # delete existing bundle
